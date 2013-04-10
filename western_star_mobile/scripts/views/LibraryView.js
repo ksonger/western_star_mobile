7df5bb85ce0078f674f93a0e0e13d88edfa23088
gomper.igbot@gmail.com <gomper.igbot@gmail.com>
2013-04-09 21:02:09 -0500

    adding more files

--- western_star_mobile/scripts/views/LibraryView.js
+++ western_star_mobile/scripts/views/LibraryView.js
@@ -7,7 +7,6 @@ window.LibraryView = StateView.extend({
 	},
 	respond:function() {
         this.$el.find("#library").css({"height":$(window).height() + "px", "width":$(window).width() + "px"});
-        //this.$el.find("#background_image").css({"width":$(window).width()+"px", "height":$(window).height()+"px"});
         this.$el.find("#library_logo").css({"left":((this.$el.find("#logo_div").width() - this.$el.find("#library_logo").width())/2) + "px", "top":((this.$el.find("#top_div").height() - this.$el.find("#logo_div").height())/2) + "px"});
         this.$el.find("#library_title").css({"top":((this.$el.find("#top_div").height() - this.$el.find("#library_title").height())/2) + "px"});
         this.$el.find("#search_div").css({"margin-top":((this.$el.find("#top_div").height() - this.$el.find("#search_div").height())/2) + "px"});
